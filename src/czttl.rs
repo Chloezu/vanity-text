7b53c6d7a1c12a6855855250233c9d8a073d7166
Chloezu <133257277+Chloezu@users.noreply.github.com>
2024-10-03 23:07:42 -0700

    okay ACTUAL last commit before github

--- src/czttl.rs
+++ src/czttl.rs
@@ -6,12 +6,17 @@ pub mod input_output {
     use std::thread;
     use std::time::Duration;
     
+    
+    // Reads file to variable
     pub fn read_text(path: &str) -> String {
         let text_file = fs::read_to_string(path)
             .expect("Error reading file!");
         return text_file;
     }
     
+    // *should* clear terminal is currently broken however.
+    // TODO: Fix the cursor not being moved to the first possible line on the terminal,
+    // it currently just starts from where the cursor's last position was
     pub fn clear_terminal() {
         print!("{}[2J", 27 as char);
         thread::sleep(Duration::from_millis(250));
@@ -19,7 +24,12 @@ pub mod input_output {
             .flush()
             .expect("Error occured in clear_terminal function")
     }
-
+    
+    // takes text {input_string} and prints it character by character 
+    // with a delay based on the {input_time} variable
+    // TODO: maybeFeature; see about maybe adding a feature to skip
+    // tab indents or chunks of spaces to allow for better reading
+    // of files like python for instance
     pub fn slow_print(input_string: &str, input_time: u64) {
         let char_vec: Vec<char> = input_string.chars().collect();
         let sleep_time = Duration::from_millis(input_time);
@@ -28,7 +38,7 @@ pub mod input_output {
             print!("{}", c);
             stdout()
                 .flush()
-                .expect("error occured in slow write function");
+                .expect("error occured in slow_print function");
             thread::sleep(sleep_time);
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-//use colored::Colorize;
-
 mod czttl;
 pub use crate::czttl::input_output;
 
@@ -7,28 +5,41 @@ use std::process;
 use std::env;
 use std::path::Path;
 
-//mod local_text;
 
 fn main() {
     
+    // TODO: fix; add help text and handling for no arguments, panics without arg
+    //
+    // is first arg a valid file?
     let args: Vec<String> = env::args().collect();
+    //println!("past arg assign");
+    
+    println!("{}", args.len());
+
+    if args.len() <= 1 {
+        println!("A file location argument is required!");
+        process::exit(1);
+    }
+    //println!("past arg empty check");
+    //println!("{:?}", args);
+
     let file_exists = Path::new(&args[1]);
     if ! file_exists.exists() {
         println!("{} is not a file!", args[1]);
         process::exit(1);
     }
 
-
-    // TODO: change this to be an argument call, so the user can put whatever file is wanted
+    // attempts to read the first argument as file
+    // if succesful, clears terminal and prints slowly
+    // otherwise exits
+    // TODO: fix; if not utf8 it currently will panic
+    // TODO: feature; add support for binary and hex read + print functionality via arguments
     let str_to_print= input_output::read_text(&args[1]);
-    
-    // This will clear the terminal with a print command
     input_output::clear_terminal(); 
-    
-    // slowly prints the text to the screen
     input_output::slow_print(&str_to_print, 50);
 
-    println!("");
+    // exits after creating newline to avoid annoyance
+    println!();
     process::exit(0);
 }
 
